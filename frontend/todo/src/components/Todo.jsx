12725426c3c2053c6ac1ec8fbbe80bf59fc9fbc0
mktintumon <mktintumon@gmail.com>
2023-11-21 15:30:10 +0530

    added

--- frontend/todo/src/components/Todo.jsx
+++ frontend/todo/src/components/Todo.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Loader from "./Loader";
 import axios from "axios";
 import "./todo.css";
 
